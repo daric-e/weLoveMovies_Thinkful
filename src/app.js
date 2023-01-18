69179d43ee027551242f16e977f5f2077bbe4417
daric-e <100322530+daric-e@users.noreply.github.com>
2023-01-17 19:34:55 -0700

    Update app.js
    Changed File names

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
 
-const notFound = require('./errors/notFound');
-const errorHandler = require('./errors/errorHandler');
+const notFound = require('./Errors/notFound');
+const errorHandler = require('./Errors/errorHandler');
 
-const moviesRouter = require('./movies/movies.router');
-const reviewsRouter = require('./reviews/reviews.router');
-const theatersRouter = require('./theaters/theaters.router');
+const moviesRouter = require('./Movies/movies.router');
+const reviewsRouter = require('./Reviews/reviews.router');
+const theatersRouter = require('./Theaters/theaters.router');
 
 const app = express();
 
